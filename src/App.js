dfc59389f8a75dd4f771d83b1405307b39bc66c4
Aleh <user@iMac-User.fios-router.home>
2020-04-12 20:44:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: 0,
+      clearId: null
+    };
+
+    this.timer = this.timer.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+  }
+  timer(e) {
+    e.preventDefault();
+    const buttonClicked = e.target.id;
+    if(buttonClicked === 'start') {
+      let id = setInterval(this.startTimer, 1000);
+      this.setState({ clearId: id });
+    } else if(buttonClicked === 'reset') {
+      this.setState({ time: 0 });
+    } else if(buttonClicked === 'pause') {
+      clearInterval(this.state.clearId);
+    }
+  }
+  startTimer() {
+      this.setState({ time: this.state.time + 1 });
+  }
+
+  render() {
+   return (
+     <div className="App">
+       <header className="App-header">
+         <h1>{this.state.time}</h1>
+         <form onClick={this.timer}>
+          <button id="start">Start</button>
+          <button id="pause">Pause</button>
+          <button id="reset">Reset</button>
+         </form>
+       </header>
+     </div>
+   );
+  }
 }
 
 export default App;
